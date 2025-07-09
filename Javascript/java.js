c7bbb54817fffc44553cf259d2abcedf386ee58b
vikash <vikashkelly@gmail.com>
2025-07-09 06:29:48 +0530

    Refactor JavaScript code for user input and calculations; add new Hindi greeting function

--- Javascript/java.js
+++ Javascript/java.js
@@ -4,12 +4,107 @@
 // document.write("Hello ",name," from ",place,"!");
 
 // take two input numbers
-let num1 = prompt("Enter first number:");
-let num2 = prompt("Enter second number:");
+// let num1 = prompt("Enter first Age: ");
+// let num2 = prompt("Enter second number:");
 
-// addtion
-document.writeln("addition: ", parseInt(num1) + parseInt(num2), "<br>");
-// multiplication
-document.writeln("Subtraction: ", num1 * num2, "<br>");
-//diviion
-document.writeln("Division: ", num1 / num2, "<br>");
+// if (num1 >= 21) {
+// 	document.writeln("Your are permited to go to pub");
+// } else {
+// 	document.writeln("Your are not elegible");
+// }
+// num1 > 21 ? document.writeln("okay") : document.writeln("problem");
+
+//
+// let age = prompt("Enter your age: ");
+
+// if (num1 >= 12 && age >= 21) {
+// 	document.writeln("you are eligible");
+// } else {
+// 	document.writeln("you are not eligible");
+// }
+
+// num1 >= 12 && age >= 21
+// 	? document.writeln("you are eligible")
+// 	: document.writeln("you are not eligible");
+//num=123, print unit 3.
+
+// let num1 = prompt("Enter the number: ");
+// b = num1 % 10;
+// document.writeln("the unit digit is ", b);
+
+// let num1 = prompt("Enter the number: ");
+// if (num1 % 2 == 0 && num1 % 3 == 0) {
+// 	document.writeln("divisible");
+// } else {
+// 	document.writeln("not divisible");
+// }
+
+// let num1 = prompt("Enter the number: ");
+// if (num1 % 2 == 0) {
+// 	if (num1 % 3 == 0) {
+// 		document.writeln("divisible");
+// 	} else {
+//         document.writeln("not divisible");
+// 	}
+// }
+//
+// let num1 = parseInt(prompt("Enter the percentage: "));
+
+// switch (num1) {
+// 	case 90:
+// 		document.writeln("A");
+// 		break;
+// 	case 70:
+// 		document.writeln("B");
+// 		break;
+// 	case 50:
+// 		document.writeln("C");
+// 		break;
+// 	default:
+//         document.writeln("F")
+// 		break;
+// }
+// loop
+
+// const x = function (a, b) { return a + b };
+// let res= x(1,3);
+
+// const y = (a, b) => a+b
+//  result=y(5,6);
+//  document.writeln(result);
+// let num1 = parseInt(prompt("Enter the number: "));
+// let a = 1;
+// for (let i = 1; i <= num1; i++) {
+// 	a = a * i;
+// }
+// document.writeln(a);
+// factorial
+// let num1 = parseInt(prompt("Enter the number: "));
+// let sum = 0;
+// while (num1 > 0) {
+// 	let a = 1;
+// 	for (let i = 1; i <= num1; i++) {
+// 		a = a * i;
+// 	}
+// 	document.writeln(a);
+// }
+// document.writeln(sum);
+
+// function fact(a) {
+// 	let fact = 1;
+// 	for (let i = 1; i <= a; i++) {
+// 		fact = fact * i;
+// 	}
+// 	document.writeln(fact);
+// }
+// let num = parseInt(prompt("Enter the number: "));
+// fact(num);
+
+str = "hello this is my string";
+let count = 0;
+for (let index = 0; i <= str.length - 1; index++) {
+	if (str[i]== " ") {
+        count++;
+    }
+}
+document.writeln(count);
